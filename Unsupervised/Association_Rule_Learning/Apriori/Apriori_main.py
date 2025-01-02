5067e78e11aeca5e973f4993417255c5b4028ab8
AnojKumarKV <kvanojkumar.ai@gmail.com>
2025-01-02 19:59:37 +0530

    committing the code

---
+++ Unsupervised/Association_Rule_Learning/Apriori/Apriori_main.py
@@ -0,0 +1,33 @@
+"""
+Calculating the support, confidence, and lift for products in a dataset using the
+apriori method through association rule based learning
+"""
+
+# Importing the libraries
+import pandas as pd
+
+# Data preprocessing
+dataset = pd.read_csv('Market_Basket_Optimisation.csv', header = None)
+transactions = []
+for i in range(0, 7501):
+  transactions.append([str(dataset.values[i,j]) for j in range(0, 20)])
+
+# Training the Apriori model on the dataset
+from apyori import apriori
+rules = apriori(transactions = transactions, min_support = 0.003, min_confidence = 0.2, min_lift = 3, min_length = 2, max_length = 2)
+
+# Displaying the result, and organizing it into a Pandas dataframe
+results = list(rules)
+
+def inspect(results):
+    lhs = [tuple(result[2][0][0])[0] for result in results]
+    rhs = [tuple(result[2][0][1])[0] for result in results]
+    supports = [result[1] for result in results]
+    confidences = [result[2][0][2] for result in results]
+    lifts = [result[2][0][3] for result in results]
+    return list(zip(lhs, rhs, supports, confidences, lifts))
+resultsinDataFrame = pd.DataFrame(inspect(results), columns = ['Left Hand Side', 'Right Hand Side', 'Support', 'Confidence', 'Lift'])
+
+# Displaying the results sorted by descending lifts upto 10 rows
+# print(resultsinDataFrame.nlargest(n = 10, columns = 'Lift'))
+
